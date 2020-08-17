ce7b7dc8cb182792d092a8444d025e0ffb6917d2
Natalia Sinitsyna (intern) <kosyak_@mail.ru>
2020-08-17 12:38:06 +0300

    Создан CitiesTableVC и поправлен AppDelegate

---
+++ AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  OpenWeatherApp
+//
+//  Created by Natali on 16.08.2020.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: - Public Properties
+    
+    var window: UIWindow?
+    var navigationController: UINavigationController?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        if let window = window {
+            let mainVC = CitiesListTableViewController()
+            navigationController = UINavigationController(rootViewController: mainVC)
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+        return true
+    }
+}
+
